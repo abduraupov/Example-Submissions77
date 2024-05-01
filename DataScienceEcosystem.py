169a862a0fe967d6a89bd5606d74a5610f277a10
abduraupov <168633920+abduraupov@users.noreply.github.com>
2024-05-01 14:43:33 +0500

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In[6]:
+
+
+print("In this notebook, Data Science Tools and Ecosystem are summarized.")
+
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. SQL
+# 3. R
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. NumPy.
+# 2. Keras.
+# 3. Pandas.
+# 4. PyTorch.
+# 5. SciPy.
+# 6. Scikit-Learn.
+# 6. TensorFlow.
+# 7. Matplotlib.
+
+# ## Data Science Tools
+# 
+# | Data science tools |
+# | --- |
+# | Python| 
+# | Apachi | 
+# | Tensor |
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+
+# In[8]:
+
+
+print("This a simple arithmetic expression to mutiply then add integers")
+
+
+# In[7]:
+
+
+(3*4)+5
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+# In[49]:
+
+
+min=int(input("Enter time in minutes"))
+h=min//60
+m=min%60
+print("Hours=",h)
+print("Minutes=",m)
+
+
+# **some of the key takeaways from the course**
+# 
+# **Objectives:**
+# 
+# __List popular languages for Data Science__
+# 
+# -  NumPy.
+# -  Keras.
+# -  Pandas.
+# 
+
+# ## Author: Name
+
+# In[ ]:
+
+
+
+
